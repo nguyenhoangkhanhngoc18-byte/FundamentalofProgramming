bc70c8b84193bdcf2b4464448821d4f73e1d6fcb
nguye <nguye@KHANHNGOC>
2026-04-30 20:02:36 +0700

    Add project files.

---
+++ FundamentalofProgramming/session1/baitap1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FundamentalofProgramming.session1
+{
+    internal class baitap1
+    {
+        /*
+1. to Add / Sum Two Numbers.
+
+2. to Swap Values of Two Variables.
+
+3. to Multiply two Floating Point Numbers
+
+4. to convert feet to meter
+
+5. to convert Celsius to Fahrenheit and vice versa
+
+6. to find the Size of data types
+
+7. to Print ASCII Value (tip: read character, print number of this char)
+
+8. to Calculate Area of Circle
+
+9. to Calculate Area of Square
+
+10. to convert days to years, weeks and days
+        */
+    }
+}
+private static void Main(string[] args)
+{
+    //1.to Add / Sum Two Numbers.
+    int a= 10 + 6
+        Console.WriteLine("10 + 6 = {0}",a)
+
+    //2. to Swap Values of Two Variables.
+
+
+}
